1bdae419f8e11d9e5e86a22069f9ca6d5e830b16
Bncardoso <107731966+Bncardoso@users.noreply.github.com>
2023-06-23 20:35:33 -0300

    Implementação dos charts na página de estatística

---
+++ src/app/pages/vendas/about-vendas/about-vendas.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about-vendas',
+  templateUrl: './about-vendas.component.html',
+  styleUrls: ['./about-vendas.component.scss']
+})
+export class AboutVendasComponent {
+
+}
